1abc3a7b11fbecc61e865c5d931c84c5c896d488
Ryan Zhu <rz.ryanzhu@gmail.com>
2024-02-17 10:27:27 -0500

    pyfirmata??

--- app.py
+++ app.py
@@ -1,36 +1,12 @@
-from arduino_iot_cloud import ArduinoCloudClient
-import os 
-from dotenv import load_dotenv
-
+import pyfirmata
 import time
-import logging
 
-import sys
-sys.path.append("lib")
+ARDUINO_PORT = "/dev/somewhere"
 
-# load environment variables
-load_dotenv()
-DEVICE_ID = os.environ.get("DEVICE_ID")
-SECRET_KEY = os.environ.get("SECRET_KEY")
+board = pyfirmata.Arduino(ARDUINO_PORT)
 
-def logging_func():
-    logging.basicConfig(
-        datefmt="%H:%M:%S",
-        format="%(asctime)s.%(msecs)03d %(message)s",
-        level=logging.INFO,
-    )   
-
-# This function is executed each time the "test_switch" variable changes 
-def on_switch_changed(client, value):
-    print("Switch Pressed! Status is: ", value)
-
-if __name__ == "__main__":
-
-    logging_func()
-    client = ArduinoCloudClient(device_id=DEVICE_ID, username=DEVICE_ID, password=SECRET_KEY)
-
-    client.register("test_value")  
-    client["test_value"] = 20
-    client.register("test_switch", value=None, on_write=on_switch_changed)
-    
-    client.start()
+while True:
+    board.digital[13].write(1)
+    time.sleep(1)
+    board.digital[13].write(0)
+    time.sleep(1)
